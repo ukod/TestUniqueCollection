c366b4080de28eab79f851e8d2d74d968c8312da
ukod <ukosname@mail.ru>
2018-01-29 22:11:13 +0300

    Example added

---
+++ UniqCollectionApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UniqCollectionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueCollection<int> uniqueList = new UniqueCollection<int>();
+
+            for (int i = 0; i < 30; i++)
+            {
+                uniqueList.Add(i);
+            }
+
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine(uniqueList[i]);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ UniqCollectionApp/UniqueCollection.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+
+namespace UniqCollectionApp
+{
+    public class UniqueCollection<T> : IList
+    {
+        const int SizeMultiplicator = 2;
+
+        const int InitialSize = 10;
+
+        public UniqueCollection()
+        {
+            if (_internalArray == null)
+                _internalArray = new T[InitialSize];
+        }
+
+        private T[] _internalArray;
+
+        public T this[int index]
+        {
+            get
+            {
+                if (_internalArray[index] != null)
+                    return _internalArray[index];
+                return default(T);
+            }
+
+            set { Add(value); }
+        }
+
+        object IList.this[int index]
+        {
+            get
+            {
+                if (_internalArray[index] != null)
+                    return _internalArray[index];
+                return null;
+            }
+
+            set { Add(value); }
+        }
+
+        public bool IsReadOnly => false;
+
+        public bool IsFixedSize => false;
+
+        public int Count
+        {
+            get { return _count; }
+            private set { _count = value; }
+        }
+
+        private int _count = 0;
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public int Add(object value)
+        {
+            if (!Contains(value))
+                _internalArray[Count] = (T)value;
+
+            var previousCount = Count;
+            var nextCount = ++Count;
+            if (nextCount >= _internalArray.Length)
+                Array.Resize<T>(ref _internalArray, Count * SizeMultiplicator);
+
+            return previousCount;
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(object value)
+        {
+            foreach (var item in _internalArray)
+                if (item.GetHashCode() == value.GetHashCode())
+                    return true;
+
+            return false;
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _internalArray.GetEnumerator();
+        }
+
+        public int IndexOf(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
